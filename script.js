975b5cd5623df80fa2be3e1a87701c554c5982e2
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2020-12-28 15:54:42 +0300

    после первого ревью

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ let popupFormElement = popup.querySelector('.popup__container');
 let popupCloseButton = popup.querySelector('.popup__close-button');
 let profileName = profile.querySelector('.profile__title');
 let profileProf = profile.querySelector('.profile__subtitle');
-let nameInput = popupFormElement.querySelector('.popup__input-name'); 
-let profInput = popupFormElement.querySelector('.popup__input-profession');
-
-function handleFormSubmit (event) {
-    event.preventDefault(); 
-    profileName.textContent = nameInput.value;
-    profileProf.textContent = profInput.value;
-    popupClose();
-}
+let nameInput = popupFormElement.querySelector('.popup__input_type_name'); 
+let profInput = popupFormElement.querySelector('.popup__input_type_profession');
 
 function popupOpen() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  profInput.value = profileProf.textContent;
 }
 
 function popupClose() {
   popup.classList.remove('popup_opened');
-  nameInput.value = profileName.textContent;
-  profInput.value = profileProf.textContent;
+}
+
+function handleFormSubmit (event) {
+  event.preventDefault(); 
+  profileName.textContent = nameInput.value;
+  profileProf.textContent = profInput.value;
+  popupClose();
 }
 
 profileEditButton.addEventListener('click', popupOpen);
